ad751c9431d34a75fe22052db2f87868eb85c68f
Ümit Arat <umitarat8098@gmail.com>
2026-05-14 12:11:32 +0300

    fix: combined urls and root redirect

--- core/urls.py
+++ core/urls.py
@@ -1,41 +1,13 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('account/', include('user.urls')),
-    path('stock/', include('stock.urls')),
-]
-
-# STATIC/MEDIA:
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# -----------------------------------------------
-# Swagger + Redoc
-from django.urls import re_path
+from django.views.generic import RedirectView # Yönlendirme için gerekli
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# --- Swagger Yapılandırması ---
 schema_view = get_schema_view(
    openapi.Info(
       title="Stock_App API",
@@ -49,10 +21,26 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+# --- Ana URL Listesi ---
+urlpatterns = [
+    # 1. Kök dizine (https://umit8103.pythonanywhere.com/) geleni swagger'a yönlendir
+    path('', RedirectView.as_view(url='swagger/', permanent=True)),
+    
+    # 2. Mevcut Uygulama Yolları
+    path('admin/', admin.site.urls),
+    path('account/', include('user.urls')),
+    path('stock/', include('stock.urls')),
 
-urlpatterns += [
-   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-   path("__debug__/", include("debug_toolbar.urls")), # django-debug-toolbar
+    # 3. Swagger & Redoc Yolları
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+# --- Debug Toolbar (Sadece Geliştirme Ortamında Çalışır) ---
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
+
+# --- Statik ve Medya Dosyaları ---
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
